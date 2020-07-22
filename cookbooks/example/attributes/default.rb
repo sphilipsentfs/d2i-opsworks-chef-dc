0498353a114ead97c1dad346ad7499ec0eee6e94
Philipsen, Stef <stef.philipsen@thermofisher.com>
2020-07-22 10:53:51 +0200

    Dependency fix

--- cookbooks/example/attributes/default.rb
+++ cookbooks/example/attributes/default.rb
@@ -3,18 +3,3 @@
 
 # Set a default name
 default['example']['name'] = 'Sam Doe'
-
-# For further information, see the Chef Infra documentation (https://docs.chef.io/attributes/).
-#
-# Cookbook:: testcookbook
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-File 'c:\user\Dimple\Desktop\test.txt' do
-  Content 'This is a test file'
-  action :create
-end
-
-windows_feature 'NET-Framework' do
-  action :install
-end
