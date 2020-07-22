68d7072187104b4235d1ef3b00d5706485fa55ef
Philipsen, Stef <stef.philipsen@thermofisher.com>
2020-07-22 11:46:55 +0200

    Removed variable from log

--- data_collector/recipes/setup.rb
+++ data_collector/recipes/setup.rb
@@ -1,7 +1,8 @@
 # This is a Chef Infra Client recipe file. It can be used to specify resources
 # which will apply configuration to a server.
 
-log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
+# log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
+log "Welcome to Chef Infra Client!" do
   level :info
 end
 
