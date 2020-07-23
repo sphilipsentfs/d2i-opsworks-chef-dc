ef67e3be009c5740a08bc40965ec0e4c861497c7
Philipsen, Stef <stef.philipsen@thermofisher.com>
2020-07-23 13:15:27 +0200

    Added Data Collector client installation

--- data_collector/recipes/setup.rb
+++ data_collector/recipes/setup.rb
@@ -29,6 +29,14 @@ windows_package 'Notepad++' do
   options '/S'
 end
 
+windows_package 'Notepad++' do
+  source 's3://d2idatacollector/DataCollector.Client.Setup.exe'
+  installer_type :custom
+  options '/quiet /log "c:\Dump\installerLog.txt" InstallationType=1 ContentFilter=1 PackageDistributor=1 EmailAddress="emailaddress@goes.here'
+end
+
+
+
 # windows_feature 'NET-Framework' do
 #   action :install
 # end
