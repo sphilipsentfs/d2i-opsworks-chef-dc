a784379b9dbba237584e7597749ca71e1f81c06c
Philipsen, Stef <stef.philipsen@thermofisher.com>
2020-07-23 14:53:15 +0200

    Add S3 dependency, change double quotes to single quotes

--- webserver_nodejs/metadata.rb
+++ webserver_nodejs/metadata.rb
@@ -1,7 +1,8 @@
-name        "webserver_nodejs"
-description "Install and configure a IIS based Node.JS webserver"
-license     "Apache 2.0"
-version     "0.0.1"
+name        'webserver_nodejs'
+description 'Install and configure a IIS based Node.JS webserver'
+license     'Apache 2.0'
+version     '0.0.1'
 
-supports    "windows"
-depends "webserver_nodejs"
+supports    'windows'
+depends     'webserver_nodejs'
+depends     's3_file'
