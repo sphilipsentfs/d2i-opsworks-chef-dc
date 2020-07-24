eea8143fa0ca59f4795182309f570feb4c8ea769
Philipsen, Stef <stef.philipsen@thermofisher.com>
2020-07-24 14:56:46 +0200

    Other S3 url

--- data_collector/recipes/setup.rb
+++ data_collector/recipes/setup.rb
@@ -33,7 +33,8 @@ end
 s3_file 'C:\Users\Public\DataCollector.Client.Setup.exe' do
   remote_path 'DataCollector.Client.Setup.exe'
   bucket 'd2idatacollector'
-  s3_url 'https://s3.amazonaws.com/bucket'
+  s3_url 'https://d2idatacollector.s3.eu-central-1.amazonaws.com'
+  # s3_url 'https://s3.amazonaws.com/bucket'
   aws_access_key_id node[:custom_access_key]
   aws_secret_access_key node[:custom_secret_key]
   action :create
